e43b2606873d2b578aa9086242bfdc2f71c89410
simzeee <jwsims816@gmail.com>
2024-12-05 17:38:33 -0500

    finish delay

---
+++ 03_closure/06_delay.js
@@ -0,0 +1,14 @@
+// ADD CODE HERE
+
+const delay = (callback, wait) => {
+  return function () {
+    setTimeout(callback, wait);
+  };
+};
+
+// UNCOMMENT THE CODE BELOW TO TEST DELAY
+let count = 0;
+const delayedFunc = delay(() => count++, 1000);
+delayedFunc();
+console.log(count); // should print '0'
+setTimeout(() => console.log(count), 1000); // should print '1' after 1 second
