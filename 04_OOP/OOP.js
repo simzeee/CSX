1522641d6799628878a2ec3fe85aeb1b34bb9e9b
simzeee <jwsims816@gmail.com>
2024-12-07 22:55:29 -0500

    make constructor make a person

--- 04_OOP/OOP.js
+++ 04_OOP/OOP.js
@@ -34,3 +34,24 @@ const sandra = personFromPersonStore("Sandra", 26);
 
 // Uncomment this line to check your work!
 sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
+
+function PersonConstructor() {
+  this.greet = function () {
+    console.log("hello");
+  };
+}
+
+function personFromConstructor(name, age) {
+  // add code here
+  const result = new PersonConstructor(name, age);
+  result.name = name;
+  result.age = age;
+  return result;
+}
+
+const mike = personFromConstructor("Mike", 30);
+
+// Uncomment these lines to check your work!
+// console.log(mike.name); // -> Logs 'Mike'
+// console.log(mike.age); // -> Logs 30
+// mike.greet(); // -> Logs 'hello'
