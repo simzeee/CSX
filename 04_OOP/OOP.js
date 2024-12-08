75d9980b8848815882de0e54f140f7e6cd83e695
simzeee <jwsims816@gmail.com>
2024-12-07 22:52:36 -0500

    finish makePerson

---
+++ 04_OOP/OOP.js
@@ -0,0 +1,15 @@
+function makePerson(name, age) {
+	// add code here
+  const result = {}
+  result.name = name
+  result.age = age
+  return result
+
+}
+
+const vicky = makePerson('Vicky', 24);
+
+
+// Uncomment these lines to check your work!
+// console.log(vicky.name); // -> Logs 'Vicky'
+// console.log(vicky.age); // -> Logs 24
