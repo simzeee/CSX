000902345ed12bc02c6019aea65afec12c02785e
simzeee <jwsims816@gmail.com>
2024-12-07 22:56:16 -0500

    add Dog

--- 04_OOP/OOP.js
+++ 04_OOP/OOP.js
@@ -36,10 +36,10 @@ const sandra = personFromPersonStore("Sandra", 26);
 sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
 
 function PersonConstructor() {
-  this.greet = function() {
-    console.log('hello');
-  }
-  this.introduce = () => console.log(`Hi, my name is ${this.name}`)
+  this.greet = function () {
+    console.log("hello");
+  };
+  this.introduce = () => console.log(`Hi, my name is ${this.name}`);
 }
 
 function personFromConstructor(name, age) {
@@ -49,10 +49,35 @@ function personFromConstructor(name, age) {
   return person;
 }
 
-const mike = personFromConstructor('Mike', 30);
+const mike = personFromConstructor("Mike", 30);
 
 // add code here
 
-
 // Uncomment this line to check your work!
 // mike.introduce(); // -> Logs 'Hi, my name is Mike'
+
+function Dog(name, breed) {
+  // add code here
+  this.name = name;
+  this.breed = breed;
+  this.tricks = [];
+}
+
+Dog.prototype.learnTrick = function (trick) {
+  this.tricks.push(trick);
+};
+
+Dog.prototype.performTrick = function (trick) {
+  if (this.tricks.includes(trick)) {
+    console.log(`${this.name} performed ${trick}!`);
+  } else {
+    console.log(`${this.name} doesn't know that trick.`);
+  }
+};
+
+const fido = new Dog("Fido", "poodle");
+
+// Uncomment these lines to check your work!
+fido.learnTrick("fetch");
+fido.performTrick("fetch"); // should log 'Fido performed fetch!'
+fido.performTrick("sit"); // should log 'Fido doesn't know that trick.'
