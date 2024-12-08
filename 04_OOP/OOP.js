d7f5ad1c3c64e9f777e2fa268be69e185634fcb1
simzeee <jwsims816@gmail.com>
2024-12-07 22:56:47 -0500

    finish Inventory

--- 04_OOP/OOP.js
+++ 04_OOP/OOP.js
@@ -81,3 +81,56 @@ const fido = new Dog("Fido", "poodle");
 fido.learnTrick("fetch");
 fido.performTrick("fetch"); // should log 'Fido performed fetch!'
 fido.performTrick("sit"); // should log 'Fido doesn't know that trick.'
+
+function Inventory(item, price) {
+  // add code here
+  this[item] = {
+    price: price,
+    item: item,
+    quantity: 1,
+  };
+}
+
+Inventory.prototype.addItem = function (item, price) {
+  if (this[item]) {
+    this[item].quantity += 1;
+    this[item].price = price;
+  } else {
+    this[item] = {
+      price: price,
+      quantity: 1,
+    };
+  }
+};
+
+Inventory.prototype.deleteItem = function (item) {
+  if (this[item]) {
+    if (this[item].quantity > 0) {
+      this[item].quantity -= 1;
+      return "Deleted";
+    } else {
+      return "Nothing to delete";
+    }
+  }
+};
+
+Inventory.prototype.checkItem = function (item) {
+  if (this[item]) {
+    return this[item];
+  } else {
+    return "Item is not in inventory";
+  }
+};
+
+const myInventory = new Inventory("cucumber", 2);
+
+// Uncomment these lines to check your work!
+myInventory.addItem("carrot", 1);
+myInventory.addItem("carrot", 1);
+console.log(myInventory);
+// console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
+// myInventory.addItem('cucumber', 3);
+// console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
+// console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
+// console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
+// console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
